87c4cc50ab8db17192612369187100bd8053547c
ParfilovaMaria <Maria.Parfilova@urfu.me>
2022-03-20 01:31:39 +0500

    Решение проблемы с выводом в RADcons и замена exe

--- RAD_cons/RadConsole.cpp
+++ RAD_cons/RadConsole.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include "RadDll.h"
 #include "VisualDLL.h"
+#include <clocale>
 
 using namespace std;
 
 int main()
 {
-	setlocale(0, "");
+	setlocale(LC_ALL, "Russian");
 	wchar_t *StName = new wchar_t[100];
 	Imya(StName);
-    cout << "Вывод данных о студенте..." << endl;
+	wcout << L"Вывод данных о студенте..." << endl;
 	cout << "__RAD DLL__" << endl;
-	cout << "Имя студента: " << endl;
+	wcout << L"Имя студента: ";
 	wcout << StName << endl;
-	cout << "Группа студента: " << endl;
+	wcout << L"Группа студента: ";
 	cout << Grouppa() << endl;
 
 	wchar_t *Name = new wchar_t[100];
 	MyName(Name);
 	cout << "__VS DLL__" << endl;
-	cout << "Имя студента: " << endl;
+	wcout << L"Имя студента: ";
 	wcout << Name << endl;
-	cout << "Группа студента: " << endl;
+	wcout << L"Группа студента: ";
 	cout << NumberOfGroup() << endl;
 	system("pause");
 	return 0;
