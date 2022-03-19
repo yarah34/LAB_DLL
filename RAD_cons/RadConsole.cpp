79326e34f17ccb8811c9470308896f131f371ca3
ParfilovaMaria <Maria.Parfilova@urfu.me>
2022-03-20 00:47:02 +0500

    Изменение вывода 2

--- RAD_cons/RadConsole.cpp
+++ RAD_cons/RadConsole.cpp
@@ -7,20 +7,21 @@ using namespace std;
 int main()
 {
 	setlocale(0, "");
-	wchar_t *StName = new wchar_t[80];
+	wchar_t *StName = new wchar_t[100];
 	Imya(StName);
+    cout << "Вывод данных о студенте..." << endl;
 	cout << "__RAD DLL__" << endl;
-	cout << "Вывод имени студента: " << endl;
+	cout << "Имя студента: " << endl;
 	wcout << StName << endl;
-	cout << "Вывод группы студента: " << endl;
+	cout << "Группа студента: " << endl;
 	cout << Grouppa() << endl;
 
-	wchar_t *Name = new wchar_t[80];
+	wchar_t *Name = new wchar_t[100];
 	MyName(Name);
 	cout << "__VS DLL__" << endl;
-	cout << "Вывод имени студента: " << endl;
+	cout << "Имя студента: " << endl;
 	wcout << Name << endl;
-	cout << "Вывод имени студента: " << endl;
+	cout << "Группа студента: " << endl;
 	cout << NumberOfGroup() << endl;
 	system("pause");
 	return 0;
